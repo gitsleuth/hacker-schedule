37fd5d44f67eebc52a2954953b9f46406f037c57
gitsleuth <154627596+gitsleuth@users.noreply.github.com>
2024-07-18 22:36:23 +0100

    Test getting shop data

--- logic.js
+++ logic.js
@@ -1,1 +1,3 @@
-﻿console.log("Test")
+﻿fetch("https://hackclub.com/_next/data/T13kD8SWUrCga_9bc9P5j/arcade/shop.json")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
